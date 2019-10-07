fcde0ad0e1c9cd993ce1676c654cc58dd5dcdd26
Alex Loskutov <alex.loskutov@gmail.com>
2019-10-07 21:44:54 +0700

    fix path

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,62 @@
 'use strict';
 
-const { src, dest } = require("gulp");
-const gulp = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
-const cssbeautify = require("gulp-cssbeautify");
-const removeCSSComments = require("gulp-strip-css-comments");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass");
-const cleancss = require("gulp-clean-css");
-const rigger = require("gulp-rigger");
-const uglify = require("gulp-uglify");
-const plumber = require("gulp-plumber");
-const imagemin = require("gulp-imagemin");
-const del = require("del");
-const panini = require("panini");
-const browsersync = require("browser-sync").create();
+const { src, dest } = require('gulp');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cssbeautify = require('gulp-cssbeautify');
+const removeCSSComments = require('gulp-strip-css-comments');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const cleancss = require('gulp-clean-css');
+const rigger = require('gulp-rigger');
+const uglify = require('gulp-uglify');
+const plumber = require('gulp-plumber');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const panini = require('panini');
+const browsersync = require('browser-sync').create();
 
 /* Paths */
 var path = {
   build: {
-    html: "dist/",
-    js: "dist/assets/js",
-    css: "dist/assets/css",
-    img: "dist/assets/img",
+    html: 'build/',
+    js: 'build/assets/js',
+    css: 'build/assets/css',
+    img: 'build/assets/img',
   },
   src: {
-    html: "src/*.html",
-    js: "src/assets/js/*.js",
-    css: "src/assets/sass/*.{scss,sass}",
-    img: "src/assets/img/**/*.{jpg,jpeg,png,svg,gif,ico}",
+    html: 'src/*.html',
+    js: 'src/assets/js/*.js',
+    css: 'src/assets/sass/*.{scss,sass}',
+    img: 'src/assets/img/**/*.{jpg,jpeg,png,svg,gif,ico}',
   },
   watch: {
-    html: "src/**/*.{html,json}",
-    js: "src/assets/js/**/*.js",
-    css: "src/assets/sass/**/*.{scss,sass}",
-    img: "src/assets/img/**/*.{jpg,jpeg,png,svg,gif,ico}"
+    html: 'src/**/*.{html,json}',
+    js: 'src/assets/js/**/*.js',
+    css: 'src/assets/sass/**/*.{scss,sass}',
+    img: 'src/assets/img/**/*.{jpg,jpeg,png,svg,gif,ico}'
   },
-  clean: "./dist"
+  clean: './build'
 };
+
 /* Vendor paths */
 var vendor = {
   build: {
-    bootstrap_css: "dist/assets/vendor/bootstrap/css",
-    bootstrap_js: "dist/assets/vendor/bootstrap/js",
-    jquery: "dist/assets/vendor/jquery/"
+    bootstrap_css: 'build/assets/vendor/bootstrap/css',
+    bootstrap_js: 'build/assets/vendor/bootstrap/js',
+    jquery: 'build/assets/vendor/jquery/'
   },
   src: {
-    bootstrap_css: "node_modules/bootstrap/dist/css/*.css",
-    bootstrap_js: "node_modules/bootstrap/dist/js/*.js",
-    jquery: "node_modules/jquery/dist/*.js"
+    bootstrap_css: 'node_modules/bootstrap/dist/css/*.css',
+    bootstrap_js: 'node_modules/bootstrap/dist/js/*.js',
+    jquery: 'node_modules/jquery/dist/*.js'
   }
 };
 
+/* Config */
 var config = {
   server: {
-    server: { baseDir: "./dist/" },
+    server: { baseDir: './build/' },
     port: 3000
   },
   panini: {
@@ -63,10 +65,15 @@ var config = {
     partials: 'src/templates/partials/',
     helpers: 'src/templates/helpers/',
     data: 'src/templates/data/'
+  },
+  autoprefixer: {
+    Browserslist: ['last 8 versions'],
+    cascade: true
+  },
+  rename: {
+    suffix: '.min'
   }
 };
-
-
 
 /* Tasks */
 function browserSync(done) {
@@ -99,7 +106,7 @@ function jquery() {
 
 function html() {
   panini.refresh();
-  return src(path.src.html, { base: "src/" })
+  return src(path.src.html, { base: 'src/' })
     .pipe(plumber())
     .pipe(panini(config.panini))
     .pipe(dest(path.build.html))
@@ -107,39 +114,32 @@ function html() {
 }
 
 function styles() {
-  return src(path.src.css, { base: "src/assets/sass" })
+  return src(path.src.css, { base: 'src/assets/sass' })
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      Browserslist: ['last 8 versions'],
-      cascade: true
-    }))
+    .pipe(autoprefixer(config.autoprefixer))
     .pipe(cssbeautify())
     .pipe(dest(path.build.css))
     .pipe(cleancss())
     .pipe(removeCSSComments())
-    .pipe(rename({
-      suffix: ".min"
-    }))
+    .pipe(rename(config.rename))
     .pipe(dest(path.build.css))
     .pipe(browsersync.stream());
 }
 
 function scripts() {
-  return src(path.src.js, { base: "src/assets/js" })
+  return src(path.src.js, { base: 'src/assets/js' })
     .pipe(plumber())
     .pipe(rigger())
     .pipe(gulp.dest(path.build.js))
     .pipe(uglify())
-    .pipe(rename({
-      suffix: ".min"
-    }))
+    .pipe(rename(config.rename))
     .pipe(dest(path.build.js))
     .pipe(browsersync.stream());
 }
 
 function images() {
-  return src(path.src.img, { base: "src/assets/img" })
+  return src(path.src.img, { base: 'src/assets/img' })
     .pipe(imagemin())
     .pipe(dest(path.build.img));
 }
